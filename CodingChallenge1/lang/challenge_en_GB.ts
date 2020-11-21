24450c7a25bab07dd24feb15ddf278a1fb903760
Greentaily <greentail@protonmail.ch>
2020-11-21 04:24:34 +0300

    CodingChallenge1 Initial l10n

---
+++ CodingChallenge1/lang/challenge_en_GB.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Cave of Linguists Coding Challenge 1</source>
+        <extracomment>Window title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="31"/>
+        <source>Challenge 1</source>
+        <extracomment>Challenge 1 tab</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="47"/>
+        <source>This will calculate volume of a sphere. Input its radius and press Calculate.</source>
+        <extracomment>Challenge 1 description</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="90"/>
+        <source> m³</source>
+        <extracomment>Challenge 1 result suffix</extracomment>
+        <translation> m³</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="103"/>
+        <source>Radius: </source>
+        <extracomment>Challenge 1 input prefix</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="106"/>
+        <source> m</source>
+        <extracomment>Challenge 1 input suffix</extracomment>
+        <translation> m</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="128"/>
+        <source>Volume:</source>
+        <extracomment>Challenge 1 result label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="160"/>
+        <source>Calculate</source>
+        <extracomment>Challenge 1 calculate button</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="184"/>
+        <source>Challenge 2</source>
+        <extracomment>Challenge 2 tab</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="256"/>
+        <source>This will display the most frequent character in a text. Enter the text and press Count.</source>
+        <extracomment>Challenge 2 description</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="272"/>
+        <source>Most frequent character(s):</source>
+        <extracomment>Challenge 2 result label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="297"/>
+        <source>Count</source>
+        <extracomment>Challenge 2 count button</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="305"/>
+        <source>Challenge 3</source>
+        <extracomment>Challenge 3 tab</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="333"/>
+        <source>Step-by-step execution:</source>
+        <extracomment>Challenge 3 result label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="340"/>
+        <source>Sort</source>
+        <extracomment>Challenge 3 Sort button</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="347"/>
+        <source>Generate</source>
+        <extracomment>Challenge 3 Generate button</extracomment>
+        <translation type="unfinished">Generate</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="361"/>
+        <source>This is a visual presentation of the selection sort algorithm. Press Generate to generate a random sequence of real numbers. Press Sort to have it sorted. The taken steps will be displayed in the text box.</source>
+        <extracomment>Challenge 3 description</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
