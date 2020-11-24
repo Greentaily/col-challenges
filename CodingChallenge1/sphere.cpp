3173fcd0f1cb54f0871a64322ee07c0e1eff0ffe
Greentaily <greentail@protonmail.ch>
2020-11-24 11:36:47 +0300

    CodingChallenge1 Sphere implementation

--- CodingChallenge1/sphere.cpp
+++ CodingChallenge1/sphere.cpp
@@ -1,6 +1,5 @@
 #include "sphere.h"
 
-
 Sphere::Sphere(QColor color_a, QColor color_b)
 	: _color_a(color_a),
 	  _color_b(color_b)
@@ -16,33 +15,90 @@ void Sphere::setRadius(qreal radius)
 
 void Sphere::randomizeColors()
 {
-
+	this->_color_a = QColor(
+						 QRandomGenerator::system()->bounded(25, 255),
+						 QRandomGenerator::system()->bounded(25, 255),
+						 QRandomGenerator::system()->bounded(25, 255));
 }
 
 QPixmap Sphere::getPixmap(int width, int height)
 {
-	const int size = qMin(width, height);
-	const double diameter = 0.8 * size;
-
 	QPixmap pixmap(width, height);
 	QPainter painter(&pixmap);
 	painter.setRenderHint(QPainter::Antialiasing);
 
-	QLinearGradient background(0, 0, 0, height);
-	background.setColorAt(0, this->_color_a);
-	background.setColorAt(1, this->_color_b);
-	QBrush brush_background(background);
-	painter.fillRect(QRect(0, 0, width, height), brush_background);
+	/* Diameter takes up 75% of the viewport space. */
+	const qreal diameter = 0.75 * qMin(width, height);
+	/* Bounding box for the sphere. */
+	const QRectF box(
+				(width - diameter) / 2.0,
+				(height - diameter) / 2.0,
+				diameter, diameter);
 
-	QPointF point_a(0.4 * size, 0.4 * size);
-	QPointF point_b(0.45 * size, 0.45 * size);
-	QRadialGradient sphere(point_a, size * 0.5, point_b, size * 0.1);
-	sphere.setColorAt(0, this->_color_a);
-	sphere.setColorAt(1, this->_color_b);
-	QBrush brush_sphere(sphere);
-	painter.setPen(Qt::NoPen);
-	painter.setBrush(brush_sphere);
-	painter.drawEllipse(QRectF((size - diameter) / 2, (size - diameter) / 2, diameter, diameter));
+	this->drawBackground(painter, width, height);
+	this->drawSphere(painter, diameter, box);
+	this->drawText(painter, diameter, box);
 
 	return pixmap;
 }
+
+void Sphere::drawBackground(QPainter& painter, int width, int height)
+{
+	QLinearGradient background(0, 0, 0, height);
+	background.setColorAt(0, this->_color_a);
+	background.setColorAt(1, this->_color_b);
+	painter.fillRect(QRect(0, 0, width, height), QBrush(background));
+}
+
+void Sphere::drawSphere(QPainter& painter, qreal diameter, QRectF box)
+{
+	/* Coordinates for these two points are calculated based on the
+	 * position of the center point of the bounding box. */
+
+	/* Center of the gradient */
+	QPointF point_a(box.center() *= 0.8);
+	/* Focal point of the gradient */
+	QPointF point_b(box.center().x() * 0.8, box.center().y() * 0.7);
+
+	QRadialGradient sphere(point_a, diameter * 0.65, point_b, diameter * 0.05);
+
+	sphere.setColorAt(0, this->_color_a);
+	sphere.setColorAt(1, this->_color_b);
+	painter.setPen(Qt::NoPen);
+	painter.setBrush(QBrush(sphere));
+
+	painter.drawEllipse(box);
+}
+
+void Sphere::drawText(QPainter& painter, qreal diameter, QRectF box)
+{
+	const qreal half_r = diameter * 0.25;
+	const qreal vertical = box.center().y();
+
+	painter.setPen(Qt::SolidLine);
+
+	/* Draw volume of the sphere */
+	painter.drawText(box, Qt::AlignCenter, "V = " + QString::number(this->_volume, 'f'));
+	/* Draw the radius value */
+	painter.save();
+	painter.translate(box.center());
+	painter.rotate(270);
+	painter.translate(-box.center().x() + half_r, - vertical - half_r * 2 - 20);
+	painter.drawText(box, Qt::AlignCenter, "r = " + QString::number(this->_radius, 'f', 2));
+	/* Draw the radius scale */
+	painter.translate(0, 10);
+	painter.drawLine(box.left() + half_r, vertical, box.right() - half_r, vertical);
+	painter.drawLine(box.left() + half_r, vertical - 5, box.left() + half_r, vertical + 5);
+	painter.drawLine(box.right() - half_r, vertical - 5, box.right() - half_r, vertical + 5);
+	/* Draw the diameter value */
+	painter.restore();
+	painter.translate(0, -half_r * 2 - 20);
+	painter.drawText(box, Qt::AlignCenter, "d = " + QString::number(this->_radius * 2, 'f', 2));
+	/* Draw the diameter scale */
+	painter.translate(0, 10);
+	painter.drawLine(box.left(), vertical, box.right(), vertical);
+	painter.drawLine(box.left(), vertical - 5, box.left(), vertical + 5);
+	painter.drawLine(box.right(), vertical - 5, box.right(), vertical + 5);
+}
+
+
--- CodingChallenge1/sphere.h
+++ CodingChallenge1/sphere.h
@@ -4,25 +4,73 @@
 #include <QPixmap>
 #include <QPainter>
 #include <QtMath>
+#include <QRandomGenerator>
 
-
+/**
+ * Class that represents sphere and handles all relevant functions.
+ */
 class Sphere
 {
 public:
 	Sphere(QColor color_a = QColorConstants::White, QColor color_b = QColorConstants::Black);
 
+	/**
+	 * @brief Set the color A for the sphere.
+	 * @details Set the top left / focal color of the gradient
+	 *			used to draw the sphere and the background.
+	 * @param color The new color.
+	 * @note Color A is supposed to be the lighter shade.
+	 *
+	 */
 	void setColorA(QColor color) {this->_color_a = color;}
+	/**
+	 * @brief Set the color B for the sphere.
+	 * @details Set the bottom right / center color of the gradient
+	 *			used to draw the sphere and the background.
+	 * @param color The new color.
+	 * @note Color B is supposed to be the darker shade.
+	 */
 	void setColorB(QColor color) {this->_color_b = color;}
+	/**
+	 * Set the radius of the sphere.
+	 * @param radius Radius of the sphere.
+	 * @note This method will also calculate and set the volume of the sphere.
+	 */
 	void setRadius(qreal radius);
+	/**
+	 * Get the radius of the sphere.
+	 * @return Radius of the sphere.
+	 */
 	qreal getRadius() {return this->_radius;}
+	/**
+	 * Get the volume of the sphere.
+	 * @return Volume of the sphere.
+	 */
 	qreal getVolume() {return this->_volume;}
 
+	/**
+	 * Generate and set new random colors for the surface of the sphere.
+	 * @note Color values are generated in range {25, 255}.
+	 */
 	void randomizeColors();
 
+	/**
+	 * @brief Get complete pixmap of the sphere.
+	 * @details Create and return pixmap of specified dimensions
+	 *			that contains center-aligned gradient representation
+	 *			of the sphere, its volume and scales for radius and diameter.
+	 * @param width Width of the requested pixmap.
+	 * @param height Height of the requested pixmap.
+	 * @return Pixmap of specified dimensions.
+	 */
 	QPixmap getPixmap(int width = 200, int height = 200);
 
 
 private:
+	void drawBackground(QPainter& painter, int width, int height);
+	void drawSphere(QPainter& painter, qreal diameter, QRectF box);
+	void drawText(QPainter& painter, qreal diameter, QRectF box);
+
 	qreal _radius = 0;
 	qreal _volume = 0;
 	QColor _color_a;
