85b6ea0a52f38518c435148636473d6ced4f094d
Greentaily <greentail@protonmail.ch>
2020-11-25 05:43:14 +0300

    CodingChallenge1 Update l10n

--- CodingChallenge1/src/lang/challenge_ru_RU.ts
+++ CodingChallenge1/src/lang/challenge_ru_RU.ts
@@ -7,7 +7,7 @@
         <location filename="../mainwindow.ui" line="14"/>
         <source>Cave of Linguists Coding Challenge 1</source>
         <extracomment>Window title</extracomment>
-        <translation type="unfinished">Пещера лингвистов Программный челлендж 1</translation>
+        <translation type="unfinished">Пещера лингвистов Coding Challenge 1</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="31"/>
@@ -19,7 +19,7 @@
         <location filename="../mainwindow.ui" line="47"/>
         <source>This will calculate volume of a sphere. Input its radius and press Calculate.</source>
         <extracomment>Challenge 1 description</extracomment>
-        <translation type="unfinished">Здесь рассчитывается объём сферы. Введите её радиус и нажмите кнопку Рассчитать.</translation>
+        <translation type="unfinished">В этой части рассчитывается объём сферы. Введите её радиус и нажмите кнопку Рассчитать.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="90"/>
@@ -28,82 +28,87 @@
         <translation> м³</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="103"/>
+        <location filename="../mainwindow.ui" line="106"/>
         <source>Radius: </source>
         <extracomment>Challenge 1 input prefix</extracomment>
-        <translation type="unfinished">Радиус: </translation>
+        <translation>Радиус: </translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="106"/>
+        <location filename="../mainwindow.ui" line="109"/>
         <source> m</source>
         <extracomment>Challenge 1 input suffix</extracomment>
         <translation> м</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="128"/>
+        <location filename="../mainwindow.ui" line="137"/>
         <source>Volume:</source>
         <extracomment>Challenge 1 result label</extracomment>
-        <translation type="unfinished">Объём:</translation>
+        <translation>Объём:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="160"/>
+        <location filename="../mainwindow.ui" line="165"/>
+        <source>Press Calculate to render the sphere.</source>
+        <translation type="unfinished">Нажмите Рассчитать чтобы визуализировать сферу.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="197"/>
         <source>Calculate</source>
         <extracomment>Challenge 1 calculate button</extracomment>
         <translation type="unfinished">Рассчитать</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="184"/>
+        <location filename="../mainwindow.ui" line="205"/>
         <source>Challenge 2</source>
         <extracomment>Challenge 2 tab</extracomment>
         <translation type="unfinished">Челлендж 2</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="256"/>
+        <location filename="../mainwindow.ui" line="277"/>
         <source>This will display the most frequent character in a text. Enter the text and press Count.</source>
         <extracomment>Challenge 2 description</extracomment>
-        <translation type="unfinished">Эта часть выводит самый частый символ в тексте. Введите текст и нажмите кнопку Посчитать.</translation>
+        <translation type="unfinished">В этой части высчитывается самый частый символ в тексте. Введите текст и нажмите кнопку Посчитать.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="272"/>
+        <location filename="../mainwindow.ui" line="293"/>
         <source>Most frequent character(s):</source>
         <extracomment>Challenge 2 result label</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Самые частые символы:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="297"/>
+        <location filename="../mainwindow.ui" line="318"/>
         <source>Count</source>
         <extracomment>Challenge 2 count button</extracomment>
         <translation type="unfinished">Посчитать</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="305"/>
+        <location filename="../mainwindow.ui" line="326"/>
         <source>Challenge 3</source>
         <extracomment>Challenge 3 tab</extracomment>
         <translation type="unfinished">Челлендж 3</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="333"/>
+        <location filename="../mainwindow.ui" line="354"/>
         <source>Step-by-step execution:</source>
         <extracomment>Challenge 3 result label</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Пошаговое выполнение:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="340"/>
+        <location filename="../mainwindow.ui" line="361"/>
         <source>Sort</source>
         <extracomment>Challenge 3 Sort button</extracomment>
         <translation type="unfinished">Сортировать</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="347"/>
+        <location filename="../mainwindow.ui" line="368"/>
         <source>Generate</source>
         <extracomment>Challenge 3 Generate button</extracomment>
         <translation type="unfinished">Сгенерировать</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="361"/>
+        <location filename="../mainwindow.ui" line="388"/>
         <source>This is a visual presentation of the selection sort algorithm. Press Generate to generate a random sequence of real numbers. Press Sort to have it sorted. The taken steps will be displayed in the text box.</source>
         <extracomment>Challenge 3 description</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Это визуальное представление сортировки выбором. Нажмите Сгенерировать, чтобы создать случайный набор чисел. Нажмите Сортировать, для сортировки. Этапы сортировки будут выведены в текстовом поле.</translation>
     </message>
 </context>
 </TS>
--- CodingChallenge1/src/main.cpp
+++ CodingChallenge1/src/main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+	QTranslator translator;
+	translator.load(QLocale(), QLatin1String("challenge"), QLatin1String("_"), QLatin1String("lang"));
+	a.installTranslator(&translator);
+
 	MainWindow w;
 	w.show();
 	return a.exec();
