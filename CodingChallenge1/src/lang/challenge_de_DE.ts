ae41086ef9940436c5409753fff9c6d89530047a
Greentaily <greentail@protonmail.ch>
2020-11-25 14:51:16 +0300

    CodingChallenge1 Update l10n

--- CodingChallenge1/src/lang/challenge_de_DE.ts
+++ CodingChallenge1/src/lang/challenge_de_DE.ts
@@ -7,19 +7,19 @@
         <location filename="../mainwindow.ui" line="14"/>
         <source>Cave of Linguists Coding Challenge 1</source>
         <extracomment>Window title</extracomment>
-        <translation type="unfinished">Höhle der Linguisten Coding Challenge 1</translation>
+        <translation>Höhle der Linguisten Coding Challenge 1</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="31"/>
         <source>Challenge 1</source>
         <extracomment>Challenge 1 tab</extracomment>
-        <translation type="unfinished">Challenge 1</translation>
+        <translation>Challenge 1</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="47"/>
         <source>This will calculate volume of a sphere. Input its radius and press Calculate.</source>
         <extracomment>Challenge 1 description</extracomment>
-        <translation type="unfinished">Das berechnet das Volumen einer Kugel. Geben Sie den Radius ein und drücken Sie Berechnen.</translation>
+        <translation>Hier wird das Volumen einer Kugel berechnet. Geben Sie einen Radius ein und drücken Sie Berechnen.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="90"/>
@@ -48,67 +48,67 @@
     <message>
         <location filename="../mainwindow.ui" line="165"/>
         <source>Press Calculate to render the sphere.</source>
-        <translation type="unfinished">Drücken Sie Berechnen um die Kugel zu darstellen.</translation>
+        <translation>Drücken Sie Berechnen um die Kugel darzustellen.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="197"/>
         <source>Calculate</source>
         <extracomment>Challenge 1 calculate button</extracomment>
-        <translation type="unfinished">Berechnen</translation>
+        <translation>Berechnen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="205"/>
         <source>Challenge 2</source>
         <extracomment>Challenge 2 tab</extracomment>
-        <translation type="unfinished">Challenge 2</translation>
+        <translation>Challenge 2</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="277"/>
         <source>This will display the most frequent character in a text. Enter the text and press Count.</source>
         <extracomment>Challenge 2 description</extracomment>
-        <translation type="unfinished">Das zeigt das häufigste Zeichen in einem Text. Geben Sie den Text ein und drücken Sie Zählen.</translation>
+        <translation>Hier wird die häufigsten Zeichen eines Textes angezeigt. Geben Sie den Text ein und drücken Sie Zählen.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="293"/>
         <source>Most frequent character(s):</source>
         <extracomment>Challenge 2 result label</extracomment>
-        <translation type="unfinished">Die häufigsten Zeichen:</translation>
+        <translation>Die häufigsten Zeichen:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="318"/>
         <source>Count</source>
         <extracomment>Challenge 2 count button</extracomment>
-        <translation type="unfinished">Zählen</translation>
+        <translation>Zählen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="326"/>
         <source>Challenge 3</source>
         <extracomment>Challenge 3 tab</extracomment>
-        <translation type="unfinished">Challenge 3</translation>
+        <translation>Challenge 3</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="354"/>
         <source>Step-by-step execution:</source>
         <extracomment>Challenge 3 result label</extracomment>
-        <translation type="unfinished">Schritt für Schritt Ausführung:</translation>
+        <translation>Schritt für Schritt Ausführung:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="361"/>
         <source>Sort</source>
         <extracomment>Challenge 3 Sort button</extracomment>
-        <translation type="unfinished">Sortieren</translation>
+        <translation>Sortieren</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="368"/>
         <source>Generate</source>
         <extracomment>Challenge 3 Generate button</extracomment>
-        <translation type="unfinished">Erzeugen</translation>
+        <translation>Erzeugen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="388"/>
         <source>This is a visual presentation of the selection sort algorithm. Press Generate to generate a random sequence of real numbers. Press Sort to have it sorted. The taken steps will be displayed in the text box.</source>
         <extracomment>Challenge 3 description</extracomment>
-        <translation type="unfinished">Das ist eine visuelle Darstellung des Selectionsort. Drücken Sie Erzeugen um randomisierte reelle Zahlen zu erzeugen. Drücken Sie Sortieren um es zu sortieren. Die einzelnen Schritte werden in der Text box gezeigt.</translation>
+        <translation>Das ist eine visuelle Darstellung des Selectionsortes. Drücken Sie Erzeugen um randomisierte reelle Zahlen zu erzeugen. Drücken Sie Sortieren um sie zu sortieren. Die einzelnen Schritte werden in der Textbox angezeigt.</translation>
     </message>
 </context>
 </TS>
